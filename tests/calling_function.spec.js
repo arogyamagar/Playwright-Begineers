499dcd8b47b4d935f817c66536a8e9e5ec749100
Arogya Thapa Magar <arogya.magar@keela.com>
2023-03-08 12:37:45 +0545

    added new spec

---
+++ tests/calling_function.spec.js
@@ -0,0 +1,6 @@
+const { hello, helloworld } = require("./demo/hello");
+
+import { hello, helloworld } from "./demo/hello";
+console.log(hello());
+
+console.log(helloworld());
--- tests/my_first_test.spec.js
+++ tests/my_first_test.spec.js
@@ -1,4 +1,5 @@
 const { test, expect } = require("@playwright/test");
+
 test("My first test", async ({ page }) => {
   await page.goto(
     "https://give-dev.keela.co/test-form-for-campaign-progress-bar-sa-1"
