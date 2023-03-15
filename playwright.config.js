316d0a6707eec776fd4d380e692ed0d57c11d58f
Binay32 <sahbinay432@gmail.com>
2023-03-15 14:13:07 +0545

    adding beforeEach to login spec

--- playwright.config.js
+++ playwright.config.js
@@ -36,8 +36,9 @@ module.exports = defineConfig({
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
+    headless:false,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+     baseURL: 'http://dev.keela.co',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,51 +1,50 @@
-import { test, expect } from "@playwright/test";
+  import { test, expect } from "@playwright/test";
 
-test("login with valid crendetial", async ({ page }) => {
-  await page.goto("https://dev.keela.co/");
-  //   await page.pause();
-  await page.getByLabel("Email").click();
-  await page.getByLabel("Email").fill("killuasociety@yopmail.com");
-  await page.getByLabel("Password").click();
-  await page.getByLabel("Password").fill("Hello@123");
-  await page.getByRole("button", { name: "Log in" }).click();
-  await expect(page.getByRole("link", { name: "Home" })).toHaveCount(1);
-});
+  test.describe("LOGIN PAGE TEST",()=>{
 
-test("login with invalid credential (valid email but invalid password)", async ({
-  page,
-}) => {
-  await page.goto("https://dev.keela.co/");
-  //   await page.pause();
-  await page.getByLabel("Email").click();
-  await page.getByLabel("Email").fill("killuasociety@yopmail.com");
-  await page.getByLabel("Password").click();
-  await page.getByLabel("Password").fill("Hello");
-  await page.getByRole("button", { name: "Log in" }).click();
-  await expect(
-    page.getByText("Login failed. Invalid email or password")
-  ).toHaveCount(1);
-});
+    test.beforeEach(async({page})=>{
+      await page.goto("https://dev.keela.co");
+      })
 
-test("login with invalid credential (invalid email but valid password)", async ({
-  page,
-}) => {
-  await page.goto("https://dev.keela.co/");
-  //   await page.pause();
-  await page.getByLabel("Email").click();
-  await page.getByLabel("Email").fill("killuasociety@gmail.com");
-  await page.getByLabel("Password").click();
-  await page.getByLabel("Password").fill("Hello@123");
-  await page.getByRole("button", { name: "Log in" }).click();
-  await expect(
-    page.getByText("Login failed. Invalid email or password")
-  ).toHaveCount(1);
-});
+  test("login with valid crendetial", async ({ page }) => {
+    await page.getByLabel("Email").click();
+    await page.getByLabel("Email").fill("killuasociety@yopmail.com");
+    await page.getByLabel("Password").click();
+    await page.getByLabel("Password").fill("Hello@123");
+    await page.getByRole("button", { name: "Log in" }).click();
+    await expect(page.getByRole("link", { name: "Home" })).toHaveCount(1);
+  });
 
-test("login with invalid email address", async ({ page }) => {
-  await page.goto("https://dev.keela.co/");
-  //   await page.pause();
-  await page.getByLabel("Email").click();
-  await page.getByLabel("Email").fill("killuasociety");
-  await page.getByLabel("Password").click();
-  await expect(page.getByText("Must be a valid email address.")).toHaveCount(1);
-});
+  test("login with invalid credential (valid email but invalid password)", async ({
+    page,
+  }) => {
+    await page.getByLabel("Email").click();
+    await page.getByLabel("Email").fill("killuasociety@yopmail.com");
+    await page.getByLabel("Password").click();
+    await page.getByLabel("Password").fill("Hello");
+    await page.getByRole("button", { name: "Log in" }).click();
+    await expect(
+      page.getByText("Login failed. Invalid email or password")
+    ).toHaveCount(1);
+  });
+
+  test("login with invalid credential (invalid email but valid password)", async ({
+    page,
+  }) => {
+    await page.getByLabel("Email").click();
+    await page.getByLabel("Email").fill("killuasociety@gmail.com");
+    await page.getByLabel("Password").click();
+    await page.getByLabel("Password").fill("Hello@123");
+    await page.getByRole("button", { name: "Log in" }).click();
+    await expect(
+      page.getByText("Login failed. Invalid email or password")
+    ).toHaveCount(1);
+  });
+
+  test("login with invalid email address", async ({ page }) => {
+    await page.getByLabel("Email").click();
+    await page.getByLabel("Email").fill("killuasociety");
+    await page.getByLabel("Password").click();
+    await expect(page.getByText("Must be a valid email address.")).toHaveCount(1);
+  });
+  });
